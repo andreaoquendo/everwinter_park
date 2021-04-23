1f04a3e3821971cf6e1c0aa25ad332801fe6a7c2
Andrea <oquendo.3455@gmail.com>
2021-04-23 20:25:34 -0300

    funcao imprime file

---
+++ imprime_file.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h> //para usar o sleep
+#define MAX_STRING 8
+
+
+void imprime_texto(char *nome_arq){
+
+    char c;
+    FILE* texto = fopen(nome_arq, "r");
+    if(texto){
+        while ((c = getc(texto)) != EOF){
+            if(c == '\\'){ // o "\" é usado como identificador de parágrafo
+                printf("\n");
+                sleep(2);
+                continue;
+            }
+            putchar(c); //printa letra por letra
+        }
+        fclose(texto);
+    }
+
+}
+
+
+int main(void)
+{
+
+    imprime_texto("0001.txt");
+
+    return 0;
+}
