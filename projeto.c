540e4907d27aa2c666032343200f9d00dd6e234f
Andrea <oquendo.3455@gmail.com>
2021-04-27 19:52:57 -0300

    reuniao 27/04

--- projeto.c
+++ projeto.c
@@ -1,5 +1,6 @@
 #include "projeto.h"
-#define TAM 10
+#define TAM 2
+#define STR 14
 #include <time.h>
 
 Grafo* criar_grafo (int tamanho) {
@@ -7,16 +8,16 @@ Grafo* criar_grafo (int tamanho) {
    Grafo *G = (Grafo *)malloc(sizeof(Grafo));
    G->arestas = 0;
    G->V = tamanho;
-   G->listaAdj = (Vertice **)malloc(tamanho * sizeof(Vertice *)); 
+   G->listaAdj = (Vertice **)malloc(tamanho * sizeof(Vertice *));
    for (v = 0; v < G->V; v++) {
       G->listaAdj[v] = NULL;
    }
    return G;
 }
 
-void inserir_aresta (Grafo *G, int u, int v, char* vect[]) { 
+void inserir_aresta (Grafo *G, int u, int v, char** vect) {
    Vertice *temp, *ultimo = NULL;
-   
+
    /* Verificando se o vértice v já existe na lista de adjacência de u: */
     for (temp = G->listaAdj[u]; temp != NULL; temp = temp->proximo) {
         if (temp->identificador == v) {
@@ -24,7 +25,7 @@ void inserir_aresta (Grafo *G, int u, int v, char* vect[]) {
         }
         ultimo = temp;
     }
-   
+
    /* Inserindo a aresta (u,v): */
     Vertice *novo = (Vertice *)malloc(sizeof(Vertice));
     novo->nome_arq = vect[v];
@@ -42,37 +43,79 @@ void inserir_aresta (Grafo *G, int u, int v, char* vect[]) {
     G->arestas++;
 }
 
-char* get_texto (int linha, FILE* arq){
-    /*obtem a string da linha*/
+void vetor_historia(char **v){
+
+    v[0] = (char*)malloc(STR*sizeof(char));
+    strcpy(v[0],"text/0001.txt");
+    v[1] = (char*)malloc(STR*sizeof(char));
+    strcpy(v[1], "text/0002.txt");
+
+    // 0 = 48, 9 = 57
+    /*int i;
+
+    for i from 0 to tam v[i] = '0000.txt';
+
+    i/10<1
+
+    for(i = 0; i < TAM; i++){
+        if(i < 10)
+            v[i] = '000'i'.txt'; // v[3] = ('0'*(i%10));
+        if(i >= 10 && i < 99)
+            v[i] = '00'i'.txt';
+    }*/
 }
 
-int conta_linhas(FILE* file){
-    // conta linhas
-}
+void escolher(char **v, Grafo* g, int id){
+    printf("\nENTREI NA ESCOLHA %d\n", id);
 
-int get_dado(Grafo *g, int linha) {
-    //return g->listaAdj[linha]->dado;
-}
+    if (g->listaAdj[id] != NULL){
+        acesso_vertice(id, v);
+        if(!g->listaAdj[id]->proximo){
+            printf("VOCÊ MORREU\n");
+            return;
+        }
+        Vertice *temp;
+        Vertice **listaTemp = (Vertice **)malloc(sizeof(Vertice));
 
+        int a = 0, i;
 
+        for(i = 0, temp = g->listaAdj[id]  ; temp != NULL ; temp = temp->proximo, i++){
+            listaTemp[i] = temp;
+            a++;
 
-int main(){
-
-    char iniciar;
-    // tela de início  que talvez fique melhor no run();
-
-    char* nomes[TAM];
-
-    imprime_texto("text/init.txt"); 
-
-    iniciar=fgetc(stdin);
-    if(iniciar==0x0A){
-        printf("\n vamos iniciar entao");
+        }
+        int ch;
+        while(scanf("%d", &ch)){
+            if(ch > 0 && ch <= a){
+                switch (ch){
+                    case 1:
+                        escolher(v, g, listaTemp[0]->identificador);
+                        break;
+                    case 2:
+                        escolher(v, g, listaTemp[1]->identificador);
+                        break;
+                    case 3:
+                        escolher(v, g, listaTemp[2]->identificador);
+                        break;
+                    case 4:
+                        escolher(v, g, listaTemp[3]->identificador);
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            }
+            else {
+                printf("ERROR[INVALID INPUT]: PLEASE CHOOSE AGAIN\n");
+            }
+        }
     }
-    return 0;
 
 }
 
+void acesso_vertice(int id, char **v){
+    imprime_texto(v[id]);
+}
 
 void imprime_texto(char *nome_arq){
 
@@ -80,7 +123,7 @@ void imprime_texto(char *nome_arq){
     FILE* texto = fopen(nome_arq, "r");
     if(texto){
         while ((c = getc(texto)) != EOF){
-            if(c == '%'){ // o "\" é usado como identificador de parágrafo
+            if(c == '%'){
                 printf("\n");
                 //sleep(2);
                 continue;
@@ -93,4 +136,33 @@ void imprime_texto(char *nome_arq){
 
     }
 
+}
+
+int main(){
+
+    char* vetor_nomes[TAM];
+    vetor_historia(vetor_nomes);
+
+    Grafo* g = criar_grafo(TAM);
+    inserir_aresta(g, 0, 0, vetor_nomes);
+    inserir_aresta(g, 0, 1, vetor_nomes);
+    inserir_aresta(g, 1, 1, vetor_nomes);
+
+    escolher(vetor_nomes, g, 0);
+
+
+
+/*
+    char iniciar;
+
+    char* nomes[TAM];
+
+    imprime_texto("text/init.txt");
+
+    iniciar=fgetc(stdin);
+    if(iniciar==0x0A){
+        printf("\n vamos iniciar entao");
+    }
+    return 0;
+*/
 }
--- projeto.h
+++ projeto.h
@@ -1,31 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* ESTRUTURA DO GRAFO*/
 
 typedef struct vertice {
-
    char* nome_arq;
    int identificador; // a posicao na lista de adj
-   struct vertice *proximo; 
+   struct vertice *proximo;
 
 } Vertice;
 
 typedef struct grafo {
-   int arestas; 
-   int V; 
-   Vertice **listaAdj; // Lista de adjacências: lista de ponteiros de vértices 
+   int arestas;
+   int V;
+   Vertice **listaAdj; // Lista de adjacências: lista de ponteiros de vértices
 } Grafo;
 
 Grafo* criar_grafo (int tamanho);
-void inserir_aresta (Grafo *G, int u, int v, char* vect[]); 
-
+void inserir_aresta (Grafo *G, int u, int v, char* vect[]);
 char* get_texto (int linha, FILE* arq); // inserir FILE como parâmetro
 int conta_linhas(FILE* file);
 int get_dado(Grafo *g, int linha);
 void liberar_grafo (Grafo *G); // ultima coisa a ser implementada
 void imprime_texto(char *nome_arq);
 void build();
+void escolher(char **v, Grafo* g, int id);
+void acesso_vertice(int id, char **v);
+void vetor_historia(char **v);
 /*
     | O que irá conter o build? |
         -Criar grafo
